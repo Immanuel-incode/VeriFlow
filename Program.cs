12150fd84bf804c222e9b42eaced3f34c55d8688
Emmanuel Onohaebi <onohaebii@gmail.com>
2026-05-30 21:11:49 +0100

    feat: Parse CSV rows into Transaction objects and store in Transaction List

--- Program.cs
+++ Program.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Transactions;
 
-namespace Transaction.Pipeline
+namespace Transaction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string inputPath = @"Database\data\raw\paysim.csv";
-            string outputPath =  @"Database\data\raw\paysim_01.csv";
-            int rowLimit = 100;
+            string csvPath = @"Database\data\raw\paysim_01.csv";
 
-            Console.WriteLine("Reading CSV file...");
+            List<Transaction> transactions = new List<Transaction>();
 
-            string[] allLines = File.ReadAllLines(inputPath);
+            string[] lines = File.ReadAllLines(csvPath);
 
-            string header =allLines[0];
+            Console.WriteLine($"Found {lines.Length - 1} transaction to process");
 
-            string[] sampleLines = new string[rowLimit + 1];
-            sampleLines[0] = header;
-
-            for (int i = 1; i <=rowLimit; i++)
+            for (int i = 1; i < lines.Length; i++)
             {
-                sampleLines[i] = allLines[i];
+                string[] values = lines[i].Split(',');
+
+                Transaction t = new Transaction();
+                t.Step = int.Parse(values[0]);
+                t.Type = values[1];
+                t.Amount = double.Parse(values[2]);
+                t.CustomerId = values[3];
+                t.OldBalanceOrig = double.Parse(values[4]);
+                t.NewBalanceOrig = double.Parse(values[5]);
+                t.MerchantId = values[6];
+                t.OldBalanceDest = double.Parse(values[7]);
+                t.NewBalanceDest = double.Parse(values[8]);
+                t.IsFraud = values[9] == "1";
+                t.IsFlaggedFraud = values[10] == "1";
+
+                transactions.Add(t);
             }
 
-            File.WriteAllLines(outputPath, sampleLines);
-
-            Console.WriteLine($"Done - {rowLimit} rows saved to {outputPath}");
+            Console.WriteLine($"Succesfully parsed {transactions.Count} transactions");
+            Console.WriteLine($"First transaction: {transactions[0].CustomerId} - {transactions[0].Type} - {transactions[0].Amount}");
         }
     }
 }
